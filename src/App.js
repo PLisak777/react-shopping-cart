a348beb057366e71eda2353fb60754ef7c43151d
Phil <philip-lisak@lambdastudents.com>
2020-09-27 14:00:39 -0700

    Created CartContext and passed into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
+
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
+
 import data from './data';
 
 // Components
@@ -12,25 +15,27 @@ function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
-	const addItem = item => {
+	const addItem = (item) => {
 		// add the given item to the cart
 		setCart([...cart, item]);
 	};
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.ProductContext value={{ cart }}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products />
-			</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+					<Route path="/cart">
+						<ShoppingCart cart={cart} />
+					</Route>
+				</div>
+			</CartContext.ProductContext>
 		</ProductContext.Provider>
 	);
 }
